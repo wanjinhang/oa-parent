3c462f64f598f52e56dbfa7f2284027e1123b61d
wansui <wanjinhang@gmail.com>
2023-12-11 15:12:39 +0800

    用户管理CRUD

--- service-oa/src/main/java/com/wansui/auth/controller/SysUserController.java
+++ service-oa/src/main/java/com/wansui/auth/controller/SysUserController.java
@@ -1,5 +1,8 @@
 package com.wansui.auth.controller;
 
+import com.alibaba.druid.util.StringUtils;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.wansui.auth.service.SysUserService;
 import com.wansui.common.result.Result;
@@ -8,10 +11,7 @@ import com.wansui.vo.system.SysUserQueryVo;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * 系统用户控制器
@@ -27,13 +27,96 @@ public class SysUserController {
     @Autowired
     private SysUserService sysUserService;
 
-    @Operation(tags = "用户条件分页查询")
+    @Operation(summary = "用户条件分页查询")
     @GetMapping("{page}/{limit}")
     public Result index(@PathVariable Long page,
                         @PathVariable Long limit,
-                        SysUserQueryVo sysUserQueryVo){
-
+                        SysUserQueryVo sysUserQueryVo) {
+        //创建page对象
         Page<SysUser> pageParam = new Page<>(page,limit);
 
+        //封装条件，判断条件值不为空
+        LambdaQueryWrapper<SysUser> wrapper = new LambdaQueryWrapper<>();
+        //获取条件值
+        String username = sysUserQueryVo.getKeyword();
+        String createTimeBegin = sysUserQueryVo.getCreateTimeBegin();
+        String createTimeEnd = sysUserQueryVo.getCreateTimeEnd();
+        //判断条件值不为空
+        //like 模糊查询
+        if(!StringUtils.isEmpty(username)) {
+            wrapper.like(SysUser::getUsername,username);
+        }
+        //ge 大于等于
+        if(!StringUtils.isEmpty(createTimeBegin)) {
+            wrapper.ge(SysUser::getCreateTime,createTimeBegin);
+        }
+        //le 小于等于
+        if(!StringUtils.isEmpty(createTimeEnd)) {
+            wrapper.le(SysUser::getCreateTime,createTimeEnd);
+        }
+
+        //调用mp的方法实现条件分页查询
+        IPage<SysUser> pageModel = sysUserService.page(pageParam, wrapper);
+        return Result.ok(pageModel);
+    }
+
+    /**
+     * 获取用户
+     *
+     * @param id ID
+     * @return @return {@link Result }
+     * @author wansui
+     * @date 2023/12/11
+     */
+    @Operation(summary  = "获取")
+    @GetMapping("get/{id}")
+    public Result get(@PathVariable Long id) {
+        SysUser user = sysUserService.getById(id);
+        return Result.ok(user);
+    }
+
+    /**
+     * 保存用户
+     *
+     * @param user 用户
+     * @return @return {@link Result }
+     * @author wansui
+     * @date 2023/12/11
+     */
+    @Operation(summary = "保存用户")
+    @PostMapping("save")
+    public Result save(@RequestBody SysUser user) {
+        sysUserService.save(user);
+        return Result.ok();
+    }
+
+    /**
+     * 按ID更新
+     *
+     * @param user 用户
+     * @return @return {@link Result }
+     * @author wansui
+     * @date 2023/12/11
+     */
+    @Operation(summary  = "按ID更新")
+    @PutMapping("update")
+    public Result updateById(@RequestBody SysUser user) {
+        sysUserService.updateById(user);
+        return Result.ok();
+    }
+
+    /**
+     * 删除
+     *
+     * @param id ID
+     * @return @return {@link Result }
+     * @author wansui
+     * @date 2023/12/11
+     */
+    @Operation(summary  = "删除")
+    @DeleteMapping("remove/{id}")
+    public Result remove(@PathVariable Long id) {
+        sysUserService.removeById(id);
+        return Result.ok();
     }
 }
